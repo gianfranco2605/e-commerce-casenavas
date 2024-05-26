7e641f2bc0d58fbff4762138602f955d97aad94d
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2024-05-26 21:45:30 +0200

    Reviews component part2

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,8 +1,9 @@
 "use client";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import Image from "next/image";
 import { useInView } from "framer-motion";
+import { cn } from "@/lib/utils";
 
 const PHONES = [
   "/testimonials/1.jpg",
@@ -15,18 +16,70 @@ const PHONES = [
 
 function splitArray<T>(array: Array<T>, numParts: number) {
   const result: Array<Array<T>> = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const index = i % numParts;
+
+    if (!result[index]) {
+      result[index] = [];
+    }
+    result[index].push(array[i]);
+  }
+
+  return result;
+}
+
+interface ReviewsInter {
+  reviews: string[];
+  className?: string;
+  reviewClassname?: (reviewIndex: number) => string;
+  msPerPixel?: number;
+}
+
+function ReviewColumns({
+  reviews,
+  className,
+  reviewClassname,
+  msPerPixel = 0,
+}: ReviewsInter) {
+  const columnRef = useRef<HTMLDivElement | null>(null);
+  const [columnsHeight, setColumnsHeight] = useState(0);
+
+  useEffect(() => {
+    if (!columnRef.current) {
+      const resizeObserver = new window.ResizeObserver(() => {
+        setColumnsHeight(columnRef.current?.offsetHeight ?? 0);
+      });
+    }
+  }, []);
+  return (
+    <div
+      ref={columnRef}
+      className={cn("animate-marquee space-y-8 py-4", className)}
+      style={{ "--marquee-duration": duration } as React.CSSProperties}
+    ></div>
+  );
 }
 
 function ReviewGrid() {
   const containerRef = useRef<HTMLDivElement | null>(null);
   const isInView = useInView(containerRef, { once: true, amount: 0.4 });
   const columns = splitArray(PHONES, 3);
+  const columns1 = columns[0];
+  const columns2 = columns[1];
+  const columns3 = splitArray(columns[0], 2);
 
   return (
     <div
       ref={containerRef}
       className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3 "
-    ></div>
+    >
+      {isInView ? (
+        <>
+          <ReviewColumns />
+        </>
+      ) : null}
+    </div>
   );
 }
 
