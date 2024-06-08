841276de119f252ea91b2da86ba227a9b63b500a
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2024-06-08 10:13:32 +0200

    las section home, footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Image from "next/image";
-import { Check, Star } from "lucide-react";
+import { ArrowRight, Check, Star } from "lucide-react";
 import Phone from "@/components/Phone";
 import { Icons } from "@/components/Icons";
 import Reviews from "@/components/Reviews";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -232,16 +234,55 @@ export default function Home() {
             </div>
           </div>
           <div className="mx-auto max-w-6xl px-6 lg:px-8">
-            <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-4">
+            <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
               <Image
                 alt=""
-                width={50}
-                height={50}
+                width={100}
+                height={100}
                 src="/arrow.png"
-                className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0"
+                className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0"
               />
+              <div className="relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl">
+                <Image
+                  src="/horse.jpg"
+                  className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full"
+                  width={200}
+                  height={200}
+                  alt="horse"
+                />
+              </div>
+              <Phone className="w-60" imgSrc="/horse_phone.jpg" />
             </div>
           </div>
+          <ul className="mx-auto mt-12 mx-w-prose sm:text-lg space-y-2 w-fit">
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              Hight-quality silicone material
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              Scatch- and fingerprints resistant coating{" "}
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              Wireless charging compatible
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />6 year
+              print warranty
+            </li>
+            <div className="flex justify-center">
+              <Link
+                className={buttonVariants({
+                  size: "lg",
+                  className: "mx-auto mt-8",
+                })}
+                href="/configure/upload"
+              >
+                Create your case now <ArrowRight className="h-4 w-4 ml-1" />{" "}
+              </Link>
+            </div>
+          </ul>
         </MaxWidthWrapper>
       </section>
     </div>
