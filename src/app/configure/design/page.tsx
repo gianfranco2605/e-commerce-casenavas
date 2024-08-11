f123c85f8581a7dd88898e9aa6310446306ac74d
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2024-08-11 12:35:05 +0200

    design page

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,3 +1,32 @@
-const Page = async () => {};
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurator from "./DesignConfigurator";
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+  //make DB call
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height } = configuration;
+
+  return <DesignConfigurator />;
+};
 
 export default Page;
